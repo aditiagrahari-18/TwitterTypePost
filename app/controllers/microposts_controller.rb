ed47b7e10c613af182dd86401198c0147e369eab
aditiagrahari-18 <aditi@akaruilabs.com>
2020-04-01 11:23:10 +0530

    modification in user feed

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,10 +1,11 @@
 class MicropostsController < ApplicationController
+  before_action :logged_in_user, only: [:create, :destroy]
   before_action :correct_user, only: :destroy
   def create
     @micropost = current_user.microposts.build(micropost_params)
     if @micropost.save
       flash[:success] = "Micropost created!"
-      redirect_to users_path
+      redirect_to root_path
       # render 'users#show'
     else
       @feed_items = current_user.feed.paginate(page: params[:page])
